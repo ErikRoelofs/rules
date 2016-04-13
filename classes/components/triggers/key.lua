c2474fb9f9695d8f44896966bafc3dda857bcec7
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2016-04-13 09:18:16 +0200

    Keytrigger uses switchboard now

--- classes/components/triggers/key.lua
+++ classes/components/triggers/key.lua
@@ -1,8 +1,11 @@
- return function(rulestate) 
+ return function(switchboard) 
   return {
-    add = function (entity, trigger, inputHandler)
-      if not rulestate.has(entity) then
-        error("Only entities with RuleState can have KeyTrigger")
+    add = function (entity, switch, trigger, inputHandler)
+      if not switchboard.has(entity) then
+        error("Only entities with switchboard can have KeyTrigger")
+      end
+      if not switchboard.get(entity):hasSwitch(switch) then
+        switchboard.get(entity):registerSwitch(switch)
       end
       local key = trigger
       local targetComponent = entity
@@ -13,16 +16,16 @@
         end,
         resolve = function(self, event)
           if event == "keydown" then
-            rulestate.get(targetComponent):setActive()
+            switchboard.get(targetComponent):setActive(switch)
           elseif event == "keyup" then
-            rulestate.get(targetComponent):setInactive()
+            switchboard.get(targetComponent):setInactive(switch)
           end
         end,
         remove = function(self)
           inputHandler:removeTriggerForKey(key, name)
         end,
         allowRemoveOtherComponent = function(self, name, component)
-          return not rulestate.isA(name, component)
+          return not switchboard.isA(name, component)
         end
       })
       inputHandler:addTriggerForKey(key, name, entity:component("triggerKey"))
--- tests/components/triggers/key.lua
+++ tests/components/triggers/key.lua
@@ -1,54 +1,54 @@
-local function testItCanOnlyAttachToComponentsWithRulestate(keytrigger, entity, rulestate, inputHandler)
-  assert( false == pcall( function() keytrigger.add(entity) end ), "Should not be possible to add a keytrigger to an entity without rulestate" )  
+local function testItCanOnlyAttachToComponentsWithswitchboard(keytrigger, entity, switchboard, inputHandler)
+  assert( false == pcall( function() keytrigger.add(entity, "someSwitch", "q", inputHandler) end ), "Should not be possible to add a keytrigger to an entity without switchboard" )  
 end
 
-local function testItBlocksRemovingRulestateFromComponents(keytrigger, entity, rulestate, inputHandler)
-  rulestate.add(entity)
-  keytrigger.add(entity, "q", inputHandler)
+local function testItBlocksRemovingswitchboardFromComponents(keytrigger, entity, switchboard, inputHandler)
+  switchboard.add(entity)
+  keytrigger.add(entity, "someSwitch", "q", inputHandler)
   
-  assert( false == pcall( function() rulestate.remove(entity) end ), "Should not be possible to remove rulestate if you have triggerKey" )
+  assert( false == pcall( function() switchboard.remove(entity) end ), "Should not be possible to remove switchboard if you have triggerKey" )
 end
 
-local function testItAddsATriggerToInputHandler(keytrigger, entity, rulestate, inputHandler)
-  rulestate.add(entity)
+local function testItAddsATriggerToInputHandler(keytrigger, entity, switchboard, inputHandler)
+  switchboard.add(entity)
   assert(inputHandler:getNumTriggersForKey("q") == 0, "Should have no triggers now")
-  keytrigger.add(entity, "q", inputHandler)  
+  keytrigger.add(entity, "someSwitch", "q", inputHandler)  
   assert(inputHandler:getNumTriggersForKey("q") == 1, "Should have one trigger now")
 end
 
-local function testOnKeyDownItWillSetComponentToActive(keytrigger, entity, rulestate, inputHandler)
-  rulestate.add(entity)
-  keytrigger.add(entity, "q", inputHandler)  
-  assert(entity:component("ruleState"):isActive() == false, "Component should be inactive now")
+local function testOnKeyDownItWillSetComponentToActive(keytrigger, entity, switchboard, inputHandler)
+  switchboard.add(entity)
+  keytrigger.add(entity, "someSwitch", "q", inputHandler)  
+  assert(entity:component("switchboard"):isActive("someSwitch") == false, "Component should be inactive now")
   inputHandler:keyDown("q")
-  assert(entity:component("ruleState"):isActive() == true, "Component should be active now")
+  assert(entity:component("switchboard"):isActive("someSwitch") == true, "Component should be active now")
 end
 
-local function testOnKeyUpItWillSetComponentToInactive(keytrigger, entity, rulestate, inputHandler)
-  rulestate.add(entity)
-  keytrigger.add(entity, "q", inputHandler)
-  entity:component("ruleState"):setActive()
-  assert(entity:component("ruleState"):isActive() == true, "Component should be active now")
+local function testOnKeyUpItWillSetComponentToInactive(keytrigger, entity, switchboard, inputHandler)
+  switchboard.add(entity)
+  keytrigger.add(entity, "someSwitch", "q", inputHandler)
+  entity:component("switchboard"):setActive("someSwitch")
+  assert(entity:component("switchboard"):isActive("someSwitch") == true, "Component should be active now")
   inputHandler:keyUp("q")
-  assert(entity:component("ruleState"):isActive() == false, "Component should be inactive now")
+  assert(entity:component("switchboard"):isActive("someSwitch") == false, "Component should be inactive now")
   
 end
 
-local function testOnRemoveItWillUnregisterKeyTrigger(keytrigger, entity, rulestate, inputHandler)
-  rulestate.add(entity)
-  keytrigger.add(entity, "q", inputHandler)
+local function testOnRemoveItWillUnregisterKeyTrigger(keytrigger, entity, switchboard, inputHandler)
+  switchboard.add(entity)
+  keytrigger.add(entity, "someSwitch", "q", inputHandler)
   keytrigger.remove(entity)
   assert(inputHandler:getNumTriggersForKey("q") == 0, "Should have no triggers now")
 end
 
 return function()
-  local r = require "classes/components/rulestate"()
+  local r = require "classes/components/switchboard"()
   local e = require "classes/core/newentity"
   local k = require "classes/components/triggers/key"(r)
   local i = require "classes/core/inputhandler"
   
-  testItCanOnlyAttachToComponentsWithRulestate(k, e(), r, i())
-  testItBlocksRemovingRulestateFromComponents(k, e(), r, i())
+  testItCanOnlyAttachToComponentsWithswitchboard(k, e(), r, i())
+  testItBlocksRemovingswitchboardFromComponents(k, e(), r, i())
   testItAddsATriggerToInputHandler(k, e(), r, i())
   testOnKeyDownItWillSetComponentToActive(k, e(), r, i())
   testOnKeyUpItWillSetComponentToInactive(k, e(), r, i())
